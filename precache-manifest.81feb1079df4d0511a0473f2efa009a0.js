e1e3f4d5f8136ac635b97c55e9a029254b0b7e83
AnjaliRawat24 <rawat.ar2418@gmail.com>
2020-10-17 01:34:42 +0530

    Updates

---
+++ precache-manifest.81feb1079df4d0511a0473f2efa009a0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e0a18929dc98d8a0e413ce45c351c9",
+    "url": "/GitCompare/index.html"
+  },
+  {
+    "revision": "eca47c86064efcb7c163",
+    "url": "/GitCompare/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "c5b29185cf72e8f65d54",
+    "url": "/GitCompare/static/css/main.e3267cd1.chunk.css"
+  },
+  {
+    "revision": "eca47c86064efcb7c163",
+    "url": "/GitCompare/static/js/2.f49127ce.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/GitCompare/static/js/2.f49127ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5b29185cf72e8f65d54",
+    "url": "/GitCompare/static/js/main.93f47ed3.chunk.js"
+  },
+  {
+    "revision": "2d717b14ccae636d1832",
+    "url": "/GitCompare/static/js/runtime-main.9738eb5e.js"
+  },
+  {
+    "revision": "4cd0bea5b8bf604a750f39a107f1cb2b",
+    "url": "/GitCompare/static/media/compare1.4cd0bea5.png"
+  },
+  {
+    "revision": "f9f941bf20919734f82688084f172742",
+    "url": "/GitCompare/static/media/icon.f9f941bf.gif"
+  },
+  {
+    "revision": "08059f05837284948d614660f17a7173",
+    "url": "/GitCompare/static/media/sort.08059f05.png"
+  }
+]);
